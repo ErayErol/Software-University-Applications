a3c8e71fd2b70eb7c5726c1985213ea74da96827
ErayErol <erayerolbmw@gmail.com>
2021-07-10 10:54:28 +0300

    Created the project and cleaned some stuff like 'using' and etc.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MessiFinder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
