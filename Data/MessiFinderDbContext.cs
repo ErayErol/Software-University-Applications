e442214dbf526ad072685e5b4a19a32ae6c2f2c9
ErayErol <erayerolbmw@gmail.com>
2021-07-10 10:53:15 +0300

    Renaming from ApplicationDbContext to MessiFinderDbContext.

---
+++ Data/MessiFinderDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace MessiFinder.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class MessiFinderDbContext : IdentityDbContext
+    {
+        public MessiFinderDbContext(DbContextOptions<MessiFinderDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
