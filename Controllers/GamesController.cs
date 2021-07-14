256a8ea984406c1efbfd3cc0878179c22021416d
ErayErol <erayerolbmw@gmail.com>
2021-07-14 12:14:28 +0300

    Added GamesController.cs

---
+++ Controllers/GamesController.cs
@@ -0,0 +1,45 @@
+﻿namespace MessiFinder.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Data;
+    using Data.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Models.Games;
+
+    public class GamesController : Controller
+    {
+        private readonly MessiFinderDbContext data;
+
+        public GamesController(MessiFinderDbContext data)
+            => this.data = data;
+
+        public IActionResult Create()
+            => View(new CreateGameFormModel
+            {
+                Playgrounds = this.GetPlaygroundViewModels()
+            });
+
+        [HttpPost]
+        public IActionResult Create(CreateGameFormModel gameForm)
+        {
+            if (ModelState.IsValid == false)
+            {
+                gameForm.Playgrounds = this.GetPlaygroundViewModels();
+
+                return View(gameForm);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private IEnumerable<GamePlaygroundViewModel> GetPlaygroundViewModels()
+            => this.data
+                .Playgrounds
+                .Select(x => new GamePlaygroundViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                }).ToList();
+    }
+}
