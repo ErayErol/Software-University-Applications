4e1a651fd342bdca1072735ce61f3aae5e5fcc87
ErayErol <erayerolbmw@gmail.com>
2021-07-14 12:28:21 +0300

    Create form and view model. Cleaned HomeController, changed Home/Index.cshtml and delete Home/Privacy.cshtml. Also changed year and title in _Layout.cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,32 +2,13 @@
 {
     using MessiFinder.Models;
     using Microsoft.AspNetCore.Mvc;
-    using Microsoft.Extensions.Logging;
     using System.Diagnostics;
 
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
---
+++ Models/Games/GamePlaygroundViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MessiFinder.Models.Games
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class GamePlaygroundViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
