5589f2b92a2806146d0706ce6f5d3282f2dc024e
ErayErol <erayerolbmw@gmail.com>
2021-07-18 15:12:40 +0300

    Created folder Utilities and added Countries

---
+++ Utilities/Countries.cs
@@ -0,0 +1,28 @@
+﻿namespace MessiFinder.Utilities
+{
+    using System.Collections.Generic;
+    using System.Globalization;
+
+    public static class Countries
+    {
+        public static IEnumerable<string> GetAll()
+        {
+            var cultureList = new List<string>();
+
+            CultureInfo[] getCultureInfo = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
+
+            foreach (var getCulture in getCultureInfo)
+            {
+                RegionInfo getRegionInfo = new RegionInfo(getCulture.LCID);
+
+                if (cultureList.Contains(getRegionInfo.EnglishName) == false)
+                {
+                    cultureList.Add(getRegionInfo.EnglishName);
+                }
+            }
+
+            cultureList.Sort();
+            return cultureList;
+        }
+    }
+}
