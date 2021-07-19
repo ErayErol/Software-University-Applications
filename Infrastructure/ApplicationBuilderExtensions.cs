9e467fb6830db0fd9c578d2e8a22702015d88441
ErayErol <erayerolbmw@gmail.com>
2021-07-19 22:56:03 +0300

    Deleted old migrations and created new

--- Infrastructure/ApplicationBuilderExtensions.cs
+++ Infrastructure/ApplicationBuilderExtensions.cs
@@ -4,6 +4,7 @@
     using Data;
     using Data.Models;
     using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
 
@@ -11,18 +12,18 @@
     {
         public static IApplicationBuilder PrepareDatabase(this IApplicationBuilder app)
         {
-            var scopeService = app.ApplicationServices.CreateScope();
+            using var scopeService = app.ApplicationServices.CreateScope();
 
             var data = scopeService.ServiceProvider.GetService<MessiFinderDbContext>();
 
             data?.Database.Migrate();
 
-            SeedPlaygrounds(data);
+            Seeds(data);
 
             return app;
         }
 
-        private static void SeedPlaygrounds(MessiFinderDbContext data)
+        private static void Seeds(MessiFinderDbContext data)
         {
             if (data.Playgrounds.Any())
             {
@@ -38,7 +39,13 @@
                     Town = "Haskovo",
                     Description = "In the summer this place is number 1 to play mini football.",
                     Address = "ул. Дунав 1 - в парка под супермаркет авеню",
-                    ImageUrl = "https://imgrabo.com/pics/businesses/b18e8a5e845a9317f4e301b3ffd58c14.jpeg"
+                    ImageUrl = "https://imgrabo.com/pics/businesses/b18e8a5e845a9317f4e301b3ffd58c14.jpeg",
+                    Cafe = true,
+                    ChangingRoom = true,
+                    Parking = true,
+                    Shower = true,
+                    PhoneNumber = "0888888889",
+                    AdminId = 1,
                 },
                 new Playground
                 {
@@ -47,7 +54,13 @@
                     Town = "Haskovo",
                     Description = "In the winter this place is number 1 to play mini football, because the players play inside.",
                     Address = "След Хотел Европа - до тенис кортовете",
-                    ImageUrl = "https://tennishaskovo.com/uploads/galerii/baza_kenana/44.jpg"
+                    ImageUrl = "https://tennishaskovo.com/uploads/galerii/baza_kenana/44.jpg",
+                    Cafe = false,
+                    ChangingRoom = true,
+                    Parking = true,
+                    Shower = true,
+                    PhoneNumber = "0888888888",
+                    AdminId = 1,
                 },
                 new Playground
                 {
@@ -56,7 +69,13 @@
                     Town = "Edirne",
                     Description = "In the summer this place is number 1 to play mini football in Edirne.",
                     Address = "Ilk Okullun yaninda.",
-                    ImageUrl = "https://hotel-evrika.com/wp-content/uploads/2019/12/VIK_6225-1024x683.jpg"
+                    ImageUrl = "https://hotel-evrika.com/wp-content/uploads/2019/12/VIK_6225-1024x683.jpg",
+                    Cafe = false,
+                    ChangingRoom = true,
+                    Parking = true,
+                    Shower = true,
+                    PhoneNumber = "0888888887",
+                    AdminId = 1,
                 },
                 new Playground
                 {
@@ -65,7 +84,13 @@
                     Town = "Plovdiv",
                     Description = "In summer and winter this place is number 1 to play mini football in Plovdiv.",
                     Address = "бул. „Асеновградско шосе",
-                    ImageUrl = "https://imgrabo.com/pics/guide/900x600/20150901162641_20158.jpg"
+                    ImageUrl = "https://imgrabo.com/pics/guide/900x600/20150901162641_20158.jpg",
+                    Cafe = false,
+                    ChangingRoom = true,
+                    Parking = true,
+                    Shower = true,
+                    PhoneNumber = "0888888886",
+                    AdminId = 1,
                 },
                 new Playground
                 {
@@ -74,7 +99,13 @@
                     Town = "Plovdiv",
                     Description = "You can workout in fitness and then play football with friends.",
                     Address = "жк. Тракия 96-Д, 4023 кв. Капитан Бураго",
-                    ImageUrl = "https://fitness-avantgarde.com/sites/default/files/img_8189.jpg"
+                    ImageUrl = "https://fitness-avantgarde.com/sites/default/files/img_8189.jpg",
+                    Cafe = false,
+                    ChangingRoom = true,
+                    Parking = true,
+                    Shower = true,
+                    PhoneNumber = "0888888885",
+                    AdminId = 1,
                 },
             });
 
