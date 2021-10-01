67ff9bc91cb37d18e997107f3e973bf4eb510e82
ErayErol <erayerolbmw@gmail.com>
2021-10-01 05:23:28 +0300

    using express instead of bodyParser

--- 2021.09 - [ JS Back End ]/02 - [ Cat Shelter - With Express ]/index.js
+++ 2021.09 - [ JS Back End ]/02 - [ Cat Shelter - With Express ]/index.js
@@ -4,7 +4,6 @@ const app = express();
 const path = require('path');
 const handlebars = require('express-handlebars');
 const upload = require('express-fileupload');
-const bodyParser = require('body-parser'); // extracts the entire body portion of an incoming request stream and exposes it on req.body
 
 //**************************** Import controllers ****************
 const catsController = require('./controllers/catsController.js');
@@ -23,8 +22,8 @@ app.set('view engine', '.hbs'); // Set file extension
 
 //**************************** Middleware **********************
 app.use(express.static('./public')); // serving static files
-app.use(bodyParser.urlencoded({ extended: false })); // support parsing of application/x-www-form-urlencoded post data
-app.use(bodyParser.json()); // support parsing of application/json type post data
+app.use(express.urlencoded({ extended: false })); // support parsing of application/x-www-form-urlencoded post data
+app.use(express.json()); // support parsing of application/json type post data
 app.use(upload()); // Upload files (like jpg, png...)
 app.use('/cats', catsController); // define route handlers
 app.use('/', homeController); // define route handlers
